8b11c7fcb7bc002bcb2ef47a83814d8931588a08
sandra <sandra.llanos.alvarez@gmail.com>
2020-12-15 04:13:29 -0500

    import and export

--- 02-intro-javascript/src/bases/import-exports.js
+++ 02-intro-javascript/src/bases/import-exports.js
@@ -2,7 +2,7 @@ import heroes, {owners} from '../data/heroes'
 
 console.log(owners);
 
-const getHeroeById = (id) => heroes.find(hero => hero.id === id );
+export const getHeroeById = (id) => heroes.find(hero => hero.id === id );
 console.log(getHeroeById(5));
 
 const getHeroeByOwner = (owner) => heroes.filter( hero => hero.owner === owner );
