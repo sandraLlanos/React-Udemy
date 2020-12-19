4f8e9a7f3e33672115a4b9a00fe5cb7b4ace052c
sandra <sandra.llanos.alvarez@gmail.com>
2020-12-19 00:56:09 -0500

    fetch

---
+++ 02-intro-javascript/src/bases/fetch.js
@@ -0,0 +1,26 @@
+// Fetch
+
+const apiKey = 'XRnsOEVygIHs5bH4hMpOall24oypnnmH';
+const request =  fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+/** this code is more defficult to understan and maintain  **/
+// request.then( response => {
+//     response.json().then(jsonResponse => {
+//         console.log(jsonResponse);
+//     })
+//     .catch(console.warn);
+// })
+// .catch(console.warn);
+
+/** It is better to use chain promises */
+
+request.then( response => response.json())
+       .then( ({ data }) => {
+           const {url} = data.images.original;
+           console.log( data.images.original.url );
+           const img = document.createElement('img');
+           img.src = url;
+           document.body.append(img);
+       })
+       .catch( console.warn )
+   
