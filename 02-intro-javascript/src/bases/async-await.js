b2d62e84c50471ecf53f73274064c1111bdadb09
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-21 23:11:36 -0500

    async-await

---
+++ 02-intro-javascript/src/bases/async-await.js
@@ -0,0 +1,28 @@
+// Async await
+
+const getImagenPromesa = () => {
+    const promise = new Promise( ( resolve, reject ) => {
+        resolve('https://anyThing')
+    })
+    return promise;
+}
+getImagenPromesa().then( console.log );
+
+// async makes a function asynchronous that returns a promise
+const getImage = async() => {
+    return 'https://anyThing-Async';
+}
+getImage().then( console.log );
+
+const getImageWithAwait = async() => {
+    const apiKey = 'XRnsOEVygIHs5bH4hMpOall24oypnnmH';
+    const resp =  await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const { data } = await resp.json();
+    const {url} = data.images.original;
+    const img = document.createElement('img');
+    img.src = url;
+    document.body.append(img);
+
+    console.log(data);
+}
+getImageWithAwait();
