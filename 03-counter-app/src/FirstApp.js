9163939d38955681a37e8a7a699ebb3190c63a24
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-25 02:39:49 -0500

    FC first-app is created

---
+++ 03-counter-app/src/FirstApp.js
@@ -0,0 +1,8 @@
+// functional component
+import React from 'react';
+
+const FirstApp = () => {
+    return <h1>Hello World!!</h1>
+}
+
+export default FirstApp;
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,11 +1,17 @@
 // Example to render element
+import React from 'react'
 import ReactDOM from 'react-dom'
+import FirstApp from './FirstApp' 
+import './index.css'
 
-console.log('Hello World!');
-const greeting = <h1>Hello World!</h1>;
-console.log(greeting);
+// console.log('Hello World!');
+// const greeting = <h1>Hello World!</h1>;
+// console.log(greeting);
+
+// const divRoot = document.querySelector('#root');
+// console.log(divRoot);
+
+// ReactDOM.render(greeting, divRoot);
 
 const divRoot = document.querySelector('#root');
-console.log(divRoot);
-
-ReactDOM.render(greeting, divRoot);
+ReactDOM.render(<FirstApp />, divRoot);
