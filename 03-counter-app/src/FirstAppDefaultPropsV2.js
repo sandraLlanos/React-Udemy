25649e3d29fa2b5f670a4b180952c39a2840a74a
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-25 23:17:23 -0500

    Default props V2

---
+++ 03-counter-app/src/FirstAppDefaultPropsV2.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FirstAppDefaultPropsV2 = ({ title, subtitle }) => {
+
+    return (
+        <>
+            <h1> { title } </h1>
+            <p> { subtitle } </p>
+        </>
+    )
+
+}
+
+FirstAppDefaultPropsV2.propTypes = {
+    // greeting: PropTypes.string,
+    greeting: PropTypes.string.isRequired,
+}
+FirstAppDefaultPropsV2.defaultProps = {
+    title: "I'm a title",
+    subtitle: "I'm a subtitle"
+}
+
+export default FirstAppDefaultPropsV2;
