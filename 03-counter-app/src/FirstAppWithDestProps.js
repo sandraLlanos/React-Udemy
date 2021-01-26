af61ebb9819b4df06f06a9573951b3afa164d1b0
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-25 23:16:06 -0500

    Destructuring props

---
+++ 03-counter-app/src/FirstAppWithDestProps.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FirstAppWithDestProps = ({ greeting }) => {
+    return (
+        <>
+            <h1> { greeting } </h1>
+            <p> My first Aplication </p>
+        </>
+    )
+
+}
+
+export default FirstAppWithDestProps;
