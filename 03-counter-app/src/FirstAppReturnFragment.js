d46d4a9bca34abbda403a8653950ee5ef710108b
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-25 23:15:05 -0500

    Fragments

---
+++ 03-counter-app/src/FirstAppReturnFragment.js
@@ -0,0 +1,20 @@
+// functional component
+
+import React, { Fragment } from 'react';
+// import React, { Fragment } from 'react';
+
+const FirstAppReturnFragment = () => {
+    return (
+        // <Fragment>
+        //     <h1> Hello Word! </h1>
+        //     <p> My first Aplication </p>
+        // </Fragment>
+        <>
+            <h1> Hello Word! </h1>
+            <p> My first Aplication </p>
+        </>
+    )
+
+}
+
+export default FirstAppReturnFragment;
