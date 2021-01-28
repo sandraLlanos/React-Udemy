6b6682deed97ddac39b68c2b0302d118f967ffbe
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-28 15:26:23 -0500

    handled events

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -2,11 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
+    
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
     return (
-        <>
+        <div className="wrapper">
            <h1>CounterApp</h1>
            <h2> { value } </h2> 
-        </>
+            <div>
+                {/* <button onClick={ (e) => handleAdd(e) }>+1</button> */}
+                <button onClick={ handleAdd }>+1</button>
+                <button>-1</button>
+            </div>
+        </div>
     )
 }
 
