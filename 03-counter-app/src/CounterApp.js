f5b36aef4983d6985e735d69e5a8bc56bada3ee1
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-28 15:57:29 -0500

    useState - Hook

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
     
-    const handleAdd = (e) => {
-        console.log(e);
+    // const state = useState('Goku');
+    // console.log( state ); // ["Goku", f]
+
+    // const [ nombre, setNombre ] = useState('Goku');
+    // console.log(nombre, setNombre);
+
+    const [counter, setCounter] = useState(0);
+    const handleAdd = () => {
+        setCounter( counter + 1 ); // I'm modifying state hook not counter constant
+        // setCounter( (count) => count + 1 ) // this is other way
     }
+    // const handleAdd = (e) => {
+    //     console.log(e);
+    // }
 
     return (
         <div className="wrapper">
            <h1>CounterApp</h1>
            <h2> { value } </h2> 
+           <h2> { counter } </h2> 
             <div>
                 {/* <button onClick={ (e) => handleAdd(e) }>+1</button> */}
                 <button onClick={ handleAdd }>+1</button>
