fa00860247ba34ab88283c520b1d1e61bf6967f5
sandra <sandra.llanos.alvarez@gmail.com>
2021-01-28 22:38:24 -0500

    first test using toBe

---
+++ 02-intro-javascript/src/tests/demo.test.js
@@ -0,0 +1,9 @@
+
+describe('First unit test with toBe', () => {
+    test('should have the same text', () => {
+        const message1 = "my first test";
+        const message2 = "my first test";
+        expect(message1).toBe(message2);
+    })    
+})
+
