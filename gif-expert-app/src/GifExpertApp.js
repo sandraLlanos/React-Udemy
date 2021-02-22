ac3e716ad064caa7500d6ff296c144a027e6f4f1
sandra <sandra.llanos.alvarez@gmail.com>
2021-02-22 01:18:29 -0500

    category creation

--- gif-expert-app/src/GifExpertApp.js
+++ gif-expert-app/src/GifExpertApp.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
+import AddCategory from './components/AddCategory';
 
 function GifExpertApp() {
     // const categories = [ 'One Punch', 'Samurai X', 'Dragon Ball'];
+    // const handleadd = () => {
+        //     setCategories([ ...categories, 'HunterXHunter']);         
+        //     // setCategories(cats => [ ...cats, 'HunterXHunter']);         
+    // }
     const [categories, setCategories] = useState( [ 'One Punch', 'Samurai X', 'Dragon Ball'] );
-    const handleadd = () => {
-        setCategories([ ...categories, 'HunterXHunter']);         
-        // setCategories(cats => [ ...cats, 'HunterXHunter']);         
-    }
     return (
         <>
             <h2>GifExpertApp</h2>
+            <AddCategory />
             <hr/> 
-            <button onClick={ handleadd }>Add</button>
+            {/* <button onClick={ handleadd }>Add</button> */}
             
             <ol>
                 {
