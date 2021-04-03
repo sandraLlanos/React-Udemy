76a3eb767d5828889df4b1d2fd3c2ba4c500f8cb
sandra llanos <sandra.llanos@inxeption.com>
2021-04-02 20:33:32 -0500

    waitForNextUpdate test

--- 04-react-gifexpertapp/src/tests/hooks/useFetchGifs.test.js
+++ 04-react-gifexpertapp/src/tests/hooks/useFetchGifs.test.js
@@ -3,12 +3,22 @@ import { useFetchGifs } from '../../hooks/useFetchGifs';
 
 describe('test in useFetchGifs hook', () => {
     
-    test('should return the initial state', () => {
-        const { result } = renderHook( () => useFetchGifs('One Punch'));
+    test('should return the initial state', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch'));
         const { data, loading } =  result.current;
         console.log(data, loading);  
+        await waitForNextUpdate();
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
+    });
+    test('should return an images array and the loading false', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs('One Punch'));
+        await waitForNextUpdate();
+        const { data, loading } =  result.current;
+        console.log(data, loading);  
+        expect( data.length ).toBe(10);
+        expect( loading ).toBe(false);
+
     });
 
 });
