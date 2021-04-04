e1a1fd2b00c1aa4e5e785251ff14f4c5aefd718d
sandra llanos <sandra.llanos@inxeption.com>
2021-04-04 18:24:42 -0500

    useEffect without cleanup

---
+++ 05-hooks-app/src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import './effects.css';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+    
+    const {name, email} = formState;
+
+    // this allows us to run a side effect when something happens to the component
+
+    useEffect( () => {
+        console.log('hey');
+    }, []);
+
+    useEffect( () => {
+        console.log('formState');
+    }, [formState]);
+    
+    useEffect( () => {
+        console.log('email');
+    }, [email]);
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value
+        })
+    }
+
+    return (
+        <>
+            <h1>UseEffect {name} </h1>
+            <hr />
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Your name"
+                    autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
+                />
+                <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="email@gmail.com"
+                    autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </>
+    )
+}
--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
     // <HookApp />,
     // <CounterApp />,
-    <CounterWithCustomHook />,
+    // <CounterWithCustomHook />,
+    <SimpleForm />,
   document.getElementById('root')
 );
 
