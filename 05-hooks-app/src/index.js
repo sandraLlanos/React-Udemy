0edf71bc763eb62a527fd5c6f9e0ac6307bcd6b9
sandra llanos <sandra.llanos@inxeption.com>
2021-04-15 16:02:58 -0500

    useLayoutEffect

--- 05-hooks-app/src/index.js
+++ 05-hooks-app/src/index.js
@@ -7,7 +7,8 @@ import ReactDOM from 'react-dom';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 
 ReactDOM.render(
     // <HookApp />,
@@ -17,7 +18,8 @@ ReactDOM.render(
     // <FormWithCustomHook />,
     // <MultipleCustomHooks />,
     // <FocusScreen />,
-    <RealExampleRef />,
+    // <RealExampleRef />,
+    <Layout />,
   document.getElementById('root')
 );
 
